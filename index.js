f7af9b0f98c56d348115f06c504a74dbef5fc3ba
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-23 09:55:41 +0200

    Jen JavaScript kod, ktery je ale mozne debugovat primo ve WebStorm.

---
+++ index.js
@@ -0,0 +1,5 @@
+let scitanec1 = 3;
+let scitanec2 = 5;
+let soucet = scitanec1 + scitanec2;
+console.log("Soucet cisel je " + soucet);
+console.log(`Soucet cisel ${scitanec1} a ${scitanec2} je ${soucet}.`); //vyuziti zpetnych apostrofu pro vlozeni hodnot promennych do textu
