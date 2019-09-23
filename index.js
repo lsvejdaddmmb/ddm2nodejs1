6b68a9412da257d1b2970cebe50ca39a68c6a9c0
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-23 10:03:50 +0200

    Webovy server vracejici jen jednoduchou webovou stranku. Po spusteni bezi na adrese localhost:8888.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let scitanec1 = 3;
-let scitanec2 = 5;
-let soucet = scitanec1 + scitanec2;
-console.log("Soucet cisel je " + soucet);
-console.log(`Soucet cisel ${scitanec1} a ${scitanec2} je ${soucet}.`); //vyuziti zpetnych apostrofu pro vlozeni hodnot promennych do textu
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.writeHead(200, {"Content-type": "text/html"});
+    res.end('<html><body>Kuk!</body></html>')
+
+}).listen(8888);
