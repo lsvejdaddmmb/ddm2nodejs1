5ee3dd4072ccb0123beff4b2d9d5531a6d2cf1d9
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-23 10:09:32 +0200

    Webovy server vraci pocet volani. Debugovanim a analyzou parametru res je potreba zjistit, proc citac skace po dvou pri volani z prohlizece.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http = require("http");
 
+let citac = 0;
+
 http.createServer((req, res) => {
+    citac++;
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end('<html><body>Kuk!</body></html>')
+    res.end(`<html><body>Pocet volani: ${citac}</body></html>`);
 
 }).listen(8888);
