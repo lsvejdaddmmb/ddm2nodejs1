24add648aa2bbfb488ea6c905b800c1aa0c7b849
Libor Svejda <Libor.Svejda@unicorn.com>
2019-09-24 16:52:55 +0200

    Citac po jedne.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const http = require("http");
 let citac = 0;
 
 http.createServer((req, res) => {
-    citac++;
+    if (req.url == "/") {
+        citac++;
+    }
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end(`<html><body>Pocet volani: ${citac}</body></html>`);
+    res.end(`<html><head><meta charset="UTF-8"></head><body>Pocet volani: ${citac}</body></html>`);
 
 }).listen(8888);
