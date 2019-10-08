584b9eb8bfcc77768c0c7c5573cc3623e32f69c2
Libor Svejda <Libor.Svejda@unicorn.com>
2019-10-08 16:59:25 +0200

    Prenos souboru index.html a obrazku do odpovedi ze serveru.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const http = require("http");
+const fs = require("fs");
 
 let citac = 0;
 
 http.createServer((req, res) => {
+    console.log(req.url);
     if (req.url == "/") {
         citac++;
-        res.writeHead(200, {"Content-type": "text/html"});
-        res.end(`<html><head><meta charset="UTF-8"></head><body>Pocet volani: ${citac}<input type="button" value="vynuluj" onclick="alert('zatim nefunguje :-P')"></body></html>`);
+        // res.writeHead(200, {"Content-type": "text/html"});
+        // let html = `<html><head><meta charset="UTF-8"></head><body>Pocet volani: ${citac}`;
+        // html = html + `<input type="button" value="vynuluj" onclick="window.location.href='/nulujcitac'">`;
+        // html = html + `</body></html>`;
+        // res
+        fs.readFile('index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
+
+    } else if (req.url.endsWith(".png")) {
+        let fileName = req.url.substr(1);
+        fs.readFile(fileName, function(err, data) {
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.write(data);
+            res.end();
+        });
+
+    } else if (req.url == "/nulujcitac") {
+        citac = 0;
+        res.writeHead(302, {"Location": "/hokuspokus"});
+        //res.end(`<html><head><meta charset="UTF-8"></head><body><h1>Citac vynulovan</h1>Pocet volani: ${citac}</body></html>`);
+        res.end();
     } else if (req.url == "/hokuspokus") {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end(`<html><head><meta charset="UTF-8"></head><body><h1>Můj hokus pokus</h1>blablabla</body></html>`);
